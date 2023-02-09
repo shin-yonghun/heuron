282c2212ba842484fa56ab1e7e12682fe7681d51
shin-yonghun <csyqwe9333@gmail.com>
2023-02-09 18:14:01 +0900

    last commit

--- src/test/java/com/org/heuron/application/service/controller/ApiRestControllerTest.java
+++ src/test/java/com/org/heuron/application/service/controller/ApiRestControllerTest.java
@@ -23,8 +23,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class ApiRestControllerTest {
